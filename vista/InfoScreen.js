a0ea18d8d4b7e4a50a8ffc14488a05407c17b516
ncs1020 <69656400+ncs1020@users.noreply.github.com>
2021-12-06 20:24:15 -0300

    Update InfoScreen.js

--- vista/InfoScreen.js
+++ vista/InfoScreen.js
@@ -7,8 +7,8 @@ function Info({ navigation }) {
     return (
       <View style={{ flex: 0.5,alignItems: 'center', justifyContent: 'center' }}>
         <Text h1> Info!</Text>
-        <Text h4>Trabajo practico Nro 2</Text>
-        <Text>DRAWER NAVIGATOR.</Text>
+        <Text h4>Trabajo practico Nro 3</Text>
+        <Text>LOGIN GOOGLE.</Text>
         <Text>Alumno: Nicolas Cohen Suaya.</Text>
         <Text>Profesor: Christian Dario Nievas.</Text>
           
